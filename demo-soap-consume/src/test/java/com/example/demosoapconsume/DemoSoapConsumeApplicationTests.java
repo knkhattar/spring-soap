c5711ac9d8fd05621118e2f1035fb2d989816423
Kamal <kk@test.com>
2021-09-12 16:11:39 +0530

    Initial version

---
+++ demo-soap-consume/src/test/java/com/example/demosoapconsume/DemoSoapConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosoapconsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSoapConsumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
